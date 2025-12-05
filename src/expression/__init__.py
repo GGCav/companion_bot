733abadb46c81e86a29e41e9218c86d0ddc8da33
GGCav <jeffreyhe406@gmail.com>
2025-12-05 17:06:41 -0500

    Revert to image displaying

--- src/expression/__init__.py
+++ src/expression/__init__.py
@@ -3,15 +3,15 @@ Expression Module
 Manages visual expressions and physical movements
 """
 
-from .eye_animator import EyeAnimator
-from .servo_controller import ServoController
+# from .eye_animator import EyeAnimator
+# from .servo_controller import ServoController
 from .emotion_display import EmotionDisplay
 from .display_renderer import DisplayRenderer
 from .transition_controller import TransitionController
 
 __all__ = [
-    'EyeAnimator',
-    'ServoController',
+    # 'EyeAnimator',
+    # 'ServoController',
     'EmotionDisplay',
     'DisplayRenderer',
     'TransitionController'
